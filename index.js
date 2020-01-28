4645d08feea238328ff5929f7053b418db9cb424
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-01-28 12:00:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+//Count number of times the website was visited
+var visits = 0;
+
 app.get('/', function (req, res) {
+   visits++;
+   console.log("Visits: " + visits);
    res.sendFile( __dirname + "/" + "index.html" );
 })
 

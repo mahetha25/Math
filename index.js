0d26631a738569ca355d4cb5412a2ea77e7bbcbd
mahetha25 <57928848+mahetha25@users.noreply.github.com>
2020-01-28 20:54:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ app.get('/', function (req, res) {
 
 serv.listen(process.env.PORT);
 console.log("Server is listening on port " + process.env.PORT);
+
+
+            function insert(num){
+                document.form.textview.value=document.form.textview.value+num
+            }
+            
+            function equal() {
+                var exp = document.form.textview.value;
+                if(exp){
+                document.form.textview.value = eval(exp)
+                }
+            }
+            function clean(){
+                document.form.textview.value="";
+            }
